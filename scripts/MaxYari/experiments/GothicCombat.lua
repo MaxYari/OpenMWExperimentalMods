0c46a2f1792da137f5db924c1261f8c209cfe4c5
Max Yari <maksyarik@gmail.com>
2023-12-26 15:58:59 +0100

    Commit just in case, moved GothicCombat work from tl file to lua file, renamed tl to not accidentally overwrite lua

--- scripts/MaxYari/experiments/GothicCombat.lua
+++ scripts/MaxYari/experiments/GothicCombat.lua
@@ -1,4 +1,6 @@
-local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 then local p, m = pcall(require, 'compat53.module'); if p then _tl_compat = m end end; local math = _tl_compat and _tl_compat.math or math
+local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 then
+   local p, m = pcall(require, 'compat53.module'); if p then _tl_compat = m end
+end; local math = _tl_compat and _tl_compat.math or math
 local input = require('openmw.input')
 local self = require('openmw.self')
 local core = require('openmw.core')
@@ -6,8 +8,15 @@ local types = require('openmw.types')
 local camera = require('openmw.camera')
 local util = require('openmw.util')
 local nearby = require('openmw.nearby')
+local animation = require('openmw.animation')
+local I = require('openmw.interfaces')
+
 
 local grazedHitDamageMult = 0.2;
+
+local attackState = nil;
+local attackGroupname = nil;
+local lastUseState = 0;
 
 local function onIHit(
    target,
@@ -15,12 +24,6 @@ local function onIHit(
    attacker,
    dmgData,
    hitConfig)
-
-
-
-
-
-
    if not dmgData.isSuccessful then
       local newDmgData = {}
 
@@ -46,7 +49,6 @@ local function onIHit(
       local castResult = nearby.castRay(position, position + direction * 200)
       local object = castResult.hitObject
       if object and object == target then
-
          newDmgData.hitPosition = castResult.hitPos
       end
 
@@ -55,14 +57,110 @@ local function onIHit(
 
 
       core.sendGlobalEvent('GCombat_grazed_hit', { damageData = newDmgData, target = target })
-
    end
 end
+
+
+-- Animation handlers --------
+------------------------------
+-- Notes: Theres no way do check what animation group is currently playing on a specific bonegroup?
+-- In the text key handler: Theres no way to know for which bonegroup the text key was triggered?
+I.AnimationController.addPlayBlendedAnimationHandler(function(groupname, options)
+   --I.AnimationController.addTextKeyHandler(nil, function(groupname, key)
+   --print("Animation text key! " .. groupname .. " : " .. key)
+   print("New animation started! " .. groupname .. " : " .. options.startkey .. " --> " .. options.stopkey)
+   -- Note: attack animation can be interrupted, so this states most likely should reset after few seconds just in case, to ponder: what if character holds the attack long enough for this to reset?
+   -- Probably need to check animation timings to figure for sure if we are in the attack group or not, can get the group during windup and then repeatedly check if its still playing, reset if not
+   if string.find(options.startkey, "chop ") then
+      -- Cancel vanilla chops, we are taking care of this
+      print("Overriding")
+      options.startkey = string.gsub(options.startkey, "chop", "chop1")
+      options.stopkey = string.gsub(options.stopkey, "chop", "chop1")
+      --[[ print("canceling")
+      animation.cancel(self, groupname) ]]
+   end
+
+   --[[ if string.find(options.startkey, "chop start") then
+      local startkey = string.gsub(options.startkey, "chop", "chop1")
+      local stopkey = string.gsub(options.stopkey, "chop", "chop1")
+
+      attackState = "windup"
+      attackGroupname = groupname
+      --slash max attack --> slash hit
+      I.AnimationController.playBlendedAnimation(groupname,
+         {
+            startkey = startkey,
+            stopkey = stopkey,
+            priority = animation.PRIORITY.Weapon
+         })
+   end ]]
+
+
+   --[[ if string.find(options.startkey, "chop max attack") then
+      print("Overriding swing anim")
+      --animation.cancel(self, groupname)
+      I.AnimationController.playBlendedAnimation('handtohand',
+         {
+            startkey = "thrust max attack",
+            stopkey = "thrust hit",
+            priority = animation.PRIORITY.Weapon
+         })
+   end
+
+   --chop small follow start --> chop small follow stop
+   if string.find(options.startkey, "chop small follow start") then
+      print("Overriding follow anim")
+      animation.cancel(self, "handtohand")
+      I.AnimationController.playBlendedAnimation('handtohand',
+         {
+            startkey = "thrust small follow start",
+            stopkey = "thrust small follow stop",
+            priority = animation.PRIORITY.Weapon
+         })
+   end ]]
+end)
+
+
+I.AnimationController.addTextKeyHandler(nil, function(groupname, key)
+   --print("Animation text key! " .. groupname .. " : " .. key)
+   --print("New animation started! " .. groupname .. " : " .. options.startkey .. " --> " .. options.stopkey)
+   -- Note: attack animation can be interrupted, so this states most likely should reset after few seconds just in case, to ponder: what if character holds the attack long enough for this to reset?
+   -- Probably need to check animation timings to figure for sure if we are in the attack group or not, can get the group during windup and then repeatedly check if its still playing, reset if not
+   -- chop small follow start --> chop small follow stop
+   --[[ if string.find(key, "chop1 hit") then
+      attackState = "follow"
+      I.AnimationController.playBlendedAnimation(attackGroupname,
+         {
+            startkey = "chop1 small follow start",
+            stopkey = "chop1 small follow stop",
+            priority = animation.PRIORITY.Weapon
+         })
+   end ]]
+
+   --[[ if string.find(key, "chop ") then
+      -- Cancel vanilla chops, we are taking care of this
+      print("canceling")
+      animation.cancel(self, groupname)
+   end ]]
+end)
+
 
 return {
    engineHandlers = {
       onUpdate = function(dt)
-
+         if lastUseState ~= self.controls.use and self.controls.use == 0 then
+            --[[ if attackState == "windup" then
+               attackState = "swing"
+               animation.cancel(self, attackGroupname)
+               I.AnimationController.playBlendedAnimation(attackGroupname,
+                  {
+                     startkey = "chop1 max attack",
+                     stopkey = "chop1 hit",
+                     priority = animation.PRIORITY.Weapon
+                  })
+            end ]]
+         end
+         lastUseState = self.controls.use
       end,
       onActorHit = function(
          target,
@@ -70,9 +168,6 @@ return {
          attacker,
          dmgData,
          hitConfig)
-
-
-
          if attacker == self.object then
             onIHit(target, weapon, attacker, dmgData, hitConfig)
          end
--- scripts/MaxYari/experiments/ImprovedAI.lua
+++ scripts/MaxYari/experiments/ImprovedAI.lua
@@ -5,6 +5,7 @@ local AI = require('openmw.interfaces').AI
 local vfs = require('openmw.vfs')
 local view = require('openmw_aux.util').deepToString
 local util = require('openmw.util')
+local I = require('openmw.interfaces')
 
 local BehaviourTree = require('behaviourtree/behaviour_tree')
 local json = require("json")
@@ -16,11 +17,14 @@ if self.object.recordId ~= "dralcea arethi" then return end
 
 -- Behaviour state of this actor --
 -----------------------------------
+
 local state = {
    -- Permanent state
    run = true,
+   attackState = "none",
+   attackGroup = nil,
    clear = function(self)
-      -- Reset every frame
+      -- Resets every frame
       self.jump = false
       self.attack = 0
       self.movement = 0
@@ -60,6 +64,10 @@ local function strToBool(str)
    else
       return false
    end
+end
+
+local function strIsBool(str)
+   return str == "true" or str == "false"
 end
 
 -- Custom behaviours ------------------
@@ -128,9 +136,9 @@ end
 
 actionRegistry['SwitchRun'] = SwitchRun
 
-function StartJump(config)
+function Jump(config)
    return BehaviourTree.Task:new({
-      rname = 'StartJump', -- rname stands for Readable Name
+      rname = 'Jump', -- rname stands for Readable Name
 
       run = function(task, state)
          state.jump = true
@@ -140,12 +148,50 @@ function StartJump(config)
       end,
 
       finish = function(task, state)
-         print("StartJump finished")
+         print("Jump task finished (in game it only started)")
       end
    })
 end
 
-actionRegistry['StartJump'] = StartJump
+actionRegistry['Jump'] = Jump
+
+function HoldAttack(config)
+   return BehaviourTree.Task:new({
+      rname = 'HoldAttack', -- rname stands for Readable Name
+
+      run = function(task, state)
+         state.attack = 1
+         task:running()
+         --task:fail()
+         --task:running()
+      end,
+
+      finish = function(task, state)
+         print("HoldAttack finished")
+      end
+   })
+end
+
+actionRegistry['HoldAttack'] = HoldAttack
+
+function ReleaseAttack(config)
+   return BehaviourTree.Task:new({
+      rname = 'ReleaseAttack', -- rname stands for Readable Name
+
+      run = function(task, state)
+         state.attack = 0
+         task:success()
+         --task:fail()
+         --task:running()
+      end,
+
+      finish = function(task, state)
+         print("ReleaseAttack finished")
+      end
+   })
+end
+
+actionRegistry['ReleaseAttack'] = ReleaseAttack
 
 function RandomOutcome(config)
    local props = config.props
@@ -180,6 +226,41 @@ function RandomCondition(config)
 end
 
 actionRegistry['RandomCondition'] = RandomCondition
+
+local function stateConditionFn(task, state)
+   local satisfied = true
+
+   for key, val in pairs(p) do
+      if strIsBool(val) then
+         val = strToBool(val)
+      end
+      if state[key] ~= val then
+         satisfied = false
+      end
+   end
+
+   return satisfied
+end
+
+function StateCondition(config)
+   local p = config.props
+   return BehaviourTree.Condition:new({
+      conditionFn = stateConditionFn,
+      node = config.node
+   })
+end
+
+actionRegistry['StateCondition'] = StateCondition
+
+function StateContinuousCondition(config)
+   local p = config.props
+   return BehaviourTree.ContinuousCondition:new({
+      conditionFn = stateConditionFn,
+      node = config.node
+   })
+end
+
+actionRegistry['StateContinuousCondition'] = StateContinuousCondition
 
 function Cooldown(config)
    local p = config.props
@@ -305,23 +386,66 @@ btree:setObject(state)
 local function onUpdate(dt)
    -- Reset everything
    state:clear()
-   -- Fill the satate
+
+   -- Fill the state
+   -- Distance to target
    local target = AI.getActiveTarget("Combat")
    if target then
       state.targetDistance = (target.position - self.object.position):length()
       --print(state.targetDistance)
    end
+   -- Is attack animation group still playing, should make the whole attack animation detection fairly robust
+   if not checkanim then
+      print("Attack group " .. state.attackGroup .. " is not playing anymore, resetting.")
+      state.attackState = "none"
+      state.attackGroup = nil
+   end
 
+   -- Run the behaviour tree!
    btree:run()
 
    self.controls.run = state.run
    self.controls.movement = state.movement
    self.controls.sideMovement = state.sideMovement
+   -- attack will never happen if this is set to 1 on the very first update, probably a bug
    self.controls.use = state.attack
    self.controls.jump = state.jump
 end
 
 
+-- Animation handlers --------
+------------------------------
+-- Notes: Theres no way do check what animation group is currently playing on a specific bonegroup?
+-- In the text key handler: Theres no way to know for which bonegroup the text key was triggered?
+I.AnimationController.addTextKeyHandler(nil, function(groupname, key)
+   print("Animation text key! " .. groupname .. " : " .. key)
+
+   -- Note: attack animation can be interrupted, so this states most likely should reset after few seconds just in case, to ponder: what if character holds the attack long enough for this to reset?
+   -- Probably need to check animation timings to figure for sure if we are in the attack group or not, can get the group during windup and then repeatedly check if its still playing, reset if not
+   if string.find(key, "chop") or string.find(key, "thrust") or string.find(key, "slash") then
+      state.attackState = "windup"
+      print("Animation attack group is ", groupname)
+      state.attackGroup = groupname
+   end
+
+   if string.find(key, "min attack") then
+      state.attackState = "min windup"
+   end
+
+   if string.find(key, "max attack") then
+      -- not correct, this will be reported both in the end of windup and beginning of attack
+      state.attackState = "max windup"
+   end
+
+   if string.find(key, "follow start") then
+      state.attackState = "swing"
+      --I.AnimationController.playBlendedAnimation('idle', { priority = anim.PRIORITY.Weapon })
+   end
+
+   if string.find(key, "follow stop") then
+      state.attackState = "none"
+   end
+end)
 
 -- Engine handlers -----------
 ------------------------------
